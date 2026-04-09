cdb089e58b12d322d94d85ea29a76f636869e6bc
nmsaikat <nmsaikat.cf@gmail.com>
2026-04-10 00:49:14 +0600

    Task 1 Done

--- task1.php
+++ task1.php
@@ -1,1 +1,23 @@
 <?php
+
+$users = [" rAHim@gmail.com ", " kArim@outlook.com ", " sUmi@yahoo.com "];
+
+foreach ($users as $user) {
+
+    // Step 1: Remove extra spaces
+    $user = trim($user);
+
+    // Step 2: Split name and domain
+    $parts = explode("@", $user);
+
+    $name = $parts[0];
+    $domain = $parts[1];
+
+    // Step 3: Format name (Title Case)
+    $name = ucfirst(strtolower($name));
+
+    // Step 4: Print output
+    echo "Name: $name, Domain: $domain\n";
+}
+
+?>
