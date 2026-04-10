10ecedc99ba79037b4849a2d8f0669576297bd7d
nmsaikat <nmsaikat.cf@gmail.com>
2026-04-10 11:51:05 +0600

    Task 3 Done

--- task3.php
+++ task3.php
@@ -1,1 +1,34 @@
 <?php
+class Vehicle {
+    // Add protected property
+    protected $brand;
+    
+    public function __construct($brand) {
+        // Initialize brand
+        $this->brand = $brand;
+    }
+}
+
+class Car extends Vehicle {
+    // Add private property
+    private $model;
+
+    public function __construct($brand, $model) {
+        // Call parent constructor
+        parent::__construct($brand);
+        
+        // Initialize model
+        $this->model = $model;
+    }
+
+    public function displayDetails() {
+        // Print brand and model
+        echo "This is a " . $this->brand . " " . $this->model . ".";
+    }
+}
+
+// Usage
+$car = new Car("Toyota", "Corolla");
+$car->displayDetails();
+
+?>
